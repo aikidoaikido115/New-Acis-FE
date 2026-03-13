f7c63b2153a303af6f2196cdb7d2ee0d2f7cdb12
Shibaitle <shiba.despacito@gmail.com>
2026-03-13 18:31:02 +0700

    feat: add new pages for EMR, support service, user manual, and warehouse with respective components

---
+++ src/app/(staff-nurse)/medicine/page.tsx
@@ -0,0 +1,5 @@
+import { MedicalManagementView } from "@/components/features/medical/MedicalManagementView";
+
+export default function MedicalPage() {
+  return <MedicalManagementView />;
+}
---
+++ src/app/(staff-nurse)/user-manual/layout.tsx
@@ -0,0 +1,10 @@
+import { ManualSidebar } from "@/components/features/user-manual/manual-sidebar";
+
+export default function ManualLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex min-h-full flex-col">
+      <ManualSidebar />
+      <main className="flex-1 p-8">{children}</main>
+    </div>
+  );
+}
