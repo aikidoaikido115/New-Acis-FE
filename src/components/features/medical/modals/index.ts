0fa123d83e7e53cb6b3fe674cf73a989f84cfca1
Shibaitle <shiba.despacito@gmail.com>
2026-04-16 00:19:17 +0700

    feat: Remove ConfirmMedicationModal export from modal index

--- src/components/features/medical/modals/index.ts
+++ src/components/features/medical/modals/index.ts
@@ -2,5 +2,4 @@ export { AddMedicationModal } from "./AddMedicationModal";
 export { EditMedicationModal } from "./EditMedicationModal";
 export { DeleteMedicationModal } from "./DeleteMedicationModal";
 export { WithholdMedicationModal } from "./WithholdMedicationModal";
-export { ConfirmMedicationModal } from "./ConfirmMedicationModal";
 export { GiveAllMedicationsModal } from "./GiveAllMedicationsModal";
