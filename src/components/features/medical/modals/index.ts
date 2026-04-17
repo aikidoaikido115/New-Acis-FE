6ce58f0b0bdc0a262da27d1c47d30f360b34d562
Shibaitle <shiba.despacito@gmail.com>
2026-04-17 17:05:06 +0700

    feat: update modal exports to use absolute paths and add form data types

--- src/components/features/medical/modals/index.ts
+++ src/components/features/medical/modals/index.ts
@@ -1,5 +1,7 @@
 export { AddMedicationModal } from "./AddMedicationModal";
-export { EditMedicationModal } from "./EditMedicationModal";
+export { EditMedicationModal } from "@/components/features/medical/modals/EditMedicationModal";
 export { DeleteMedicationModal } from "./DeleteMedicationModal";
 export { WithholdMedicationModal } from "./WithholdMedicationModal";
 export { GiveAllMedicationsModal } from "./GiveAllMedicationsModal";
+export type { AddMedicationFormData } from "./AddMedicationModal";
+export type { EditMedicationFormData } from "@/components/features/medical/modals/EditMedicationModal";
