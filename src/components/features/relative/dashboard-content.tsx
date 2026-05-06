c750cc4862b81cd66b6b1050add8a5da67e498b3
thiyada-kitt <pthiyada.kittiwithitkun@gmail.com>
2026-05-06 20:41:42 +0700

    relative: w-max-xxx to w-max-full

--- src/components/features/relative/dashboard-content.tsx
+++ src/components/features/relative/dashboard-content.tsx
@@ -93,7 +93,7 @@ export function RelativeDashboardContent({
   };
 
   return (
-    <div className="max-w-6xl mx-auto space-y-6">
+    <div className="max-w-full space-y-6">
       {/* Preview Banner */}
       {showPreviewBanner && residentName && (
         <div className="bg-yellow-50 border border-yellow-200 rounded-lg p-3">
